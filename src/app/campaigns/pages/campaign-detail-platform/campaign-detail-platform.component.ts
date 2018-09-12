9a6566c3dc9b17a9f804f06b079f07db3db392a8
-KaiK- <enriqueoriol@gmail.com>
2018-09-12 15:02:03 +0200

    Navigating to 404 in case platform does not exist

--- src/app/campaigns/pages/campaign-detail-platform/campaign-detail-platform.component.ts
+++ src/app/campaigns/pages/campaign-detail-platform/campaign-detail-platform.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Platform } from '../../models/platform';
 import { CampaignDetailComponent } from '../campaign-detail/campaign-detail.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 /**
@@ -22,7 +22,7 @@ export class CampaignDetailPlatformComponent implements OnInit, OnDestroy {
   platform: Platform;
   campaignSubscription: Subscription;
 
-  constructor(private parent: CampaignDetailComponent, private route: ActivatedRoute) {
+  constructor(private parent: CampaignDetailComponent, private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit() {
@@ -30,6 +30,9 @@ export class CampaignDetailPlatformComponent implements OnInit, OnDestroy {
       const name = params.get('platform');
       this.campaignSubscription = this.parent.campaign$.subscribe(campaign => {
         this.platform = campaign.platforms.get(name);
+        if (!this.platform) {
+          this.router.navigate(['/404']);
+        }        
       });
     });
   }
